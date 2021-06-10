5e0adb9f409b4b721f99f9e0c13e68932a0c72c8
frontendpolitecnico2021 <82734047+frontendpolitecnico2021@users.noreply.github.com>
2021-06-09 20:28:33 -0500

    Update SocketServer.java
    Se mejora documentación del código

--- src/s14_socket_server/SocketServer.java
+++ src/s14_socket_server/SocketServer.java
@@ -1,59 +1,103 @@
 package s14_socket_server;
 
-import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 
-public class SocketServer extends Thread {
-	   private ServerSocket serverSocket;
-	   
-	   public SocketServer(int port) throws IOException {
-	      serverSocket = new ServerSocket(port);
-	      //serverSocket.setSoTimeout(10000);
-	   }
+/**
+ * SocketServer --- Programa que actua como servidor de peticiones del Banco Politécnico.
+ * @author   SubGrupo 16 - Persistencia de Datos
+ * 
+ */
 
+public class SocketServer extends Thread {
+	
+	   /** Instacia de la clase Server Socket del paquete de utilidades de red de Java */
+	   private ServerSocket serverSocket;   
+	  
+	   /** Puerto Definido para escuchar peticiones o solicitudes  */
+	   private static int PORT = 3000;
+	   
+	   /** Tiempo de sessión del servidor de sockets en milisegundos */
+	   private static int TIMESESSION = 0;
+	   
+	   
+	   /**
+	    * Constructor de la clase SocketServer
+	    * @param int port. Puerto de ejecución del servidor de sockets
+	    * */
+	   public SocketServer(int port) throws IOException {
+	      serverSocket = new ServerSocket(port);	    
+	   }// Cierre del constructor
+
+	   /**
+	    * Este método indica al intérprete de Java que cree un 
+	    * contexto del hilo del sistema y comience a ejecutarlo
+	    * 
+	    * */
 	   public void run() {
+		   // Bucle que se ejecuta como daemon mientras no se genere un error 
 	      while(true) {
 	         try {
-	            System.out.println("Waiting for client on port " + 
+	          // Imprime en consola  la conexión al puerto definido 
+	        	 System.out.println("Conectando al servidor por el puerto: " + 
 	               serverSocket.getLocalPort() + "...");
+	        	 // Acepta peticiones del socket
 	            Socket server = serverSocket.accept();
 	            
-	            System.out.println("Just connected to " + server.getRemoteSocketAddress());
+	            // habilita o deshabilita la opción timeout con el valor 
+	            // de tiempo de espera dado, en milisegundos
+	            server.setSoTimeout(TIMESESSION);
+	            
+	            // Imprime en consola el cliente conectado 
+	            System.out.println("Cliente conectado  desde: " + server.getRemoteSocketAddress());
 	            DataInputStream in = new DataInputStream(server.getInputStream());
 	            
-	            System.out.println("Lectura Client: " + in.readUTF());
+	            // Imprime en consola la lectura del cliente
+	            System.out.println("Lectura Cliente: " + in.readUTF());
 	            DataOutputStream out = new DataOutputStream(server.getOutputStream());
+	            // Escritura del servidor cuando termina la ejecución del servidor
+	            out.writeUTF("Gracias por conectarse al servidor " + server.getLocalSocketAddress()
+	               + "\nHasta Luego!");
 	            
-	          //  BufferedReader readRequest = new BufferedReader(new InputStreamReader(server.getInputStream()));
-	            out.writeUTF("Thank you for connecting to " + server.getLocalSocketAddress()
-	               + "\nGoodbye!");
+	            // Cierra la ejecución del servidor de sockets
 	            server.close();
 	            
 	         } catch (SocketTimeoutException s) {
-	            System.out.println("Socket timed out!");
+	            System.out.println("Timeout del servidor!");
 	            break;
-	         } catch (IOException e) {
+	         } catch (IOException e) {	        
 	            e.printStackTrace();
 	            break;
 	         }
+	         catch (Exception e) {
+	        	 e.printStackTrace();
+		            break;
+			}
 	      }
-	   }
-	   
-	   public static void main(String [] args) {
-	    //  int port = Integer.parseInt(args[0]);
-		   
-		   int port = 3000;
+	   }// Cierra método run
+	   /**
+	    * Método para iniciar la aplicación
+	    * */
+	   public static void main(String [] args) {		   
+	
+	   //Captura exception cuando se genere un error
 	      try {
-	         Thread t = new SocketServer(port);
+	    	  
+	    	  // Ejecución de la clase por medio de hilos donde llama al constructor y 
+	    	  // recibe el puerto de ejecución
+	         Thread t = new SocketServer(PORT);
+	         
+	         // Inicia la ejecución del hilo
 	         t.start();
 	      } catch (IOException e) {
 	         e.printStackTrace();
 	      }
-	   }
-	}
+	      catch (Exception e) {
+	    	  e.printStackTrace();
+		}
+	   } // Cierre del método main
+	}// Cierre de la clase SocketServer
